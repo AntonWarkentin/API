51778ca0b9fc9442356252dc65c8ff8f437fb12b
Anton Warkentin <reddoshoruda@gmail.com>
2023-07-05 18:32:07 +0500

    +

--- Home_16/APITests/BaseApiTest.cs
+++ Home_16/APITests/BaseApiTest.cs
@@ -1,17 +1,22 @@
-﻿using Home_16.Services;
+﻿using Allure.Net.Commons;
+using Home_16.Services;
+using NUnit.Allure.Core;
 
 namespace Home_16.APITests
 {
+    [AllureNUnit]
     internal class BaseApiTest
     {
         protected ProjectService projectService;
         protected TestCaseService testCaseService;
+        private AllureLifecycle allure;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
             projectService = new ProjectService();
             testCaseService = new TestCaseService();
+            allure = AllureLifecycle.Instance;
         }
     }
 }
